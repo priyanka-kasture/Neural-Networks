26f96305d325ff4737b13eb2118ac2ac70ed6859
Priyanka Kasture <42208426+priyanka-kasture@users.noreply.github.com>
2018-09-07 10:39:52 +0530

    Update AdditionOfTwoNumbers.py

--- Introduction/AdditionOfTwoNumbers.py
+++ Introduction/AdditionOfTwoNumbers.py
@@ -19,16 +19,16 @@ Here tensorflow has created a graph.
 But it does not run the graph till you create an instance of a session and then run the same. 
 Before creating and running the session, if we try printing value of C, the output will be a tensor, not actual value of C.
 '''
-print("Value of C before running tensor:",C)
+print("Value of C before running the session: ",C)
 
-''' Value of C before running tensor: Tensor("C_1:0", shape=(), dtype=int32) '''
+''' Value of C before running the session: Tensor("C_1:0", shape=(), dtype=int32) '''
 
 # An instance of a session is created using tf.Session() call.
 sess = tf.Session()
 
 # Now we run the session and store the results of the computation in 'output'
 output = sess.run(C)
-print("Value of C after running graph:",output)
+print("Value of C after running the session: ",output)
 
 # Closing the session
 sess.close()
